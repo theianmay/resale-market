77831ca46127497d7152ad027603b818fdf5c1a4
Ian <ianmayw@gmail.com>
2023-12-07 10:40:46 -0500

    Add vector icons

--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { View } from 'react-native';
 import { Image, StyleSheet } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 import colors from '../config/colors';
 
 function ViewImageScreen(props) {
     return (
         <View style={styles.container}>
-        <View style={styles.closeIcon}></View>
-        <View style={styles.deleteIcon}></View>
+            <View style={styles.closeIcon}>
+                <MaterialCommunityIcons name="close-thick" size={50} />
+        </View>
+            <View style={styles.deleteIcon}>
+                <MaterialCommunityIcons name="delete" size={50} />
+
+        </View>
         <Image
             resizeMode='contain'
             style={styles.image}
