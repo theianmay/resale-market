6ee0d93bbcd92a39e62d5304e6bf1ab0763495da
Ian <ianmayw@gmail.com>
2025-07-09 15:31:19 -0700

    Add listings screen, update screen component

--- App.js
+++ App.js
@@ -8,11 +8,12 @@ import ViewImageScreen from './app/screens/ViewImageScreen';
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 import MessagesScreen from './app/screens/MessagesScreen';
 import AccountScreen from './app/screens/AccountScreen';
+import ListingsScreen from './app/screens/ListingsScreen';
 
 export default function App() {
   return (
     <GestureHandlerRootView>
-      <AccountScreen />
+      <ListingsScreen />
     </GestureHandlerRootView>
   )
 }
