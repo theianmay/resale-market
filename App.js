977bb9a584d830cbd6d761217e56e79187e47fbd
Ian <ianmayw@gmail.com>
2026-01-01 12:30:53 -0800

    Add login screen

--- App.js
+++ App.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState } from "react";
+import { Switch, TextInput, View } from "react-native";
 
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
-import WelcomeScreen from './app/screens/WelcomeScreen';
-import ViewImageScreen from './app/screens/ViewImageScreen';
-import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
-import MessagesScreen from './app/screens/MessagesScreen';
-import AccountScreen from './app/screens/AccountScreen';
-import ListingsScreen from './app/screens/ListingsScreen';
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
+import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
+import MessagesScreen from "./app/screens/MessagesScreen";
+import AccountScreen from "./app/screens/AccountScreen";
+import ListingsScreen from "./app/screens/ListingsScreen";
+import LoginScreen from "./app/screens/LoginScreen";
+
+import Screen from "./app/components/Screen";
 
 export default function App() {
-  return (
-    <GestureHandlerRootView>
-      <ListingsScreen />
-    </GestureHandlerRootView>
-  )
+  return <LoginScreen />;
 }
--- app/components/AppTextInput.js
+++ app/components/AppTextInput.js
@@ -1,36 +1,38 @@
-import React from 'react';
-import { TextInput, View, StyleSheet } from 'react-native';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import React from "react";
+import { TextInput, View, StyleSheet } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-import defaultStyles from '../config/styles';
+import defaultStyles from "../config/styles";
 
 function AppTextInput({ icon, ...otherProps }) {
-    return (
-        <div>
-            <View style={styles.container}>
-                {icon && <MaterialCommunityIcons
-                    name={icon}
-                    size={20}
-                    color={defaultStyles.colors.gray}
-                    style={styles.icon} />}
-                <TextInput style={defaultStyles.text} {...otherProps} />
-            </View>
-        </div>
-    );
+  return (
+    <View style={styles.container}>
+      {icon && (
+        <MaterialCommunityIcons
+          name={icon}
+          size={20}
+          color={defaultStyles.colors.gray}
+          style={styles.icon}
+        />
+      )}
+      <TextInput style={defaultStyles.text} {...otherProps} />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: defaultStyles.colors.lightgray,
-        borderRadius: 25,
-        flexDirection: "row",
-        width: '100%',
-        padding: 15,
-        marginVertical: 10,
-    },
-    icon: {
-        marginRight: 10,
-    }
-})
+  container: {
+    backgroundColor: defaultStyles.colors.lightgray,
+    borderRadius: 25,
+    flexDirection: "row",
+    width: "100%",
+    padding: 15,
+    marginVertical: 10,
+  },
+  icon: {
+    marginRight: 10,
+    alignSelf: "center",
+  },
+});
 
 export default AppTextInput;
